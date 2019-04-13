ea044a478d08240692013a484e27baff6d5f16aa
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-04-13 17:41:31 +0000

    Update docs after building Travis build 3 of zblanks/gen_hc
    The docs were built from the branch 'master' against the commit
    e532d43e5bb3511ec2f33ed0d3f2604547dc03a2.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/zblanks/gen_hc/jobs/519710960.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post5+ge78f67c',
+    VERSION: '0.post6+ge532d43',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
